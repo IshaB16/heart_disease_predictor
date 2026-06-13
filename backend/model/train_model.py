15d4bb7ba79f2a5aedaaaec84444d10666eccd8a
IshaB16 <bhangre2006isha@gmail.com>
2026-06-13 23:06:00 +0530

    add styling and finalize app

--- backend/model/train_model.py
+++ backend/model/train_model.py
@@ -7,7 +7,7 @@ import joblib
 
 df = pd.read_csv("heart_disease_combined.csv")
 
-X = df.drop("target", axis=1)  # adjust column name if different
+X = df.drop(["target", "source"], axis=1)
 y = df["target"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
